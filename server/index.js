d85788a73fc860f9a9499a001e0816f6683f1ba5
Younes Outerbah <younesouterbah1@gmail.com>
2024-01-24 23:48:59 +0100

    intializing redux toolkit

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import authRoutes from "./routes/authRoutes.js";
 import usersRoutes from "./routes/usersRoutes.js";
 import postsRoutes from "./routes/postsRoutes.js";
 import { verifyToken } from "./middleware/authMiddleware.js";
+import Post from "./models/Post.js";
+import User from "./models/User.js";
+import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
